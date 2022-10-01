76fc4e5d8f645bb29022cbe35c2456d1d860cd4e
Gabriel Marcolino Silva <66142528+Gabriel-MarcoSilva@users.noreply.github.com>
2022-10-01 15:07:04 -0300

    feat: termino do modulo B, só passei por cima, já compreendia boa parte

---
+++ JavaScript/moduloB/script.js
@@ -0,0 +1,36 @@
+//window.alert("minha prmeira mensagem")
+//window.confirm("está gostando? ")
+
+function nome(){
+    var nome = window.prompt("qual seu nome?")
+    document.getElementById("name").innerHTML = nome;
+    //ou
+    //document.write(`o nome:   ${nome}  tem  ${nome.length}  letras`)
+}
+
+//typeof
+    /*
+        retorna o tipo da variavel
+    */
+
+//parse
+    /*
+        transforma o tipo de uma variável para outro
+        parseInt, parseFloat, parseBoolean, String
+        se quiser colocar Number.parseInt
+    */
+
+//toFixed(n) -> serve pra coocar as casas decimais
+
+
+//operadores
+
+    /*
+        Aritméticos: 
+            + soma
+            - subtração
+            * multiplicação
+            / divisao
+            % resto
+            ** exponenciação
+    */
