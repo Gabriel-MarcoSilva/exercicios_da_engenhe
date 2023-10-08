40db6d2e939131d1bcdf9cc75b4a1a7592bccb00
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-10-08 15:44:58 -0300

    feat: arquivos do robocode

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -6,7 +6,9 @@ const BatalhaSumoController = require('../app/controller/BatalhaSumoController')
 const VitoriaController = require('../app/controller/VitoriaController')
 const CorridaFollowController = require('../app/controller/CorridaFollowController')
 const BatalhaMiniController = require('../app/controller/BatalhaMiniController')
+const BatalhaCodeController = require('../app/controller/BatalhaCodeController')
 const VitoriaMiniController = require('../app/controller/VitoriaMiniController')
+const VitoriaCodeController = require('../app/controller/VitoriaCodeController')
 
 routes.get('/', (req, res) => {
     res.status(200).json({
@@ -30,6 +32,11 @@ routes.get("/round-mini", BatalhaMiniController.list)
 routes.delete("/delet-round-mini/:_id", BatalhaMiniController.delete)
 routes.get("/round-mini/:_id", BatalhaMiniController.byId)
 
+routes.post("/round-code", BatalhaCodeController.create)
+routes.get("/round-code", BatalhaCodeController.list)
+routes.delete("/delet-round-code/:_id", BatalhaCodeController.delete)
+routes.get("/round-code/:_id", BatalhaCodeController.byId)
+
 routes.post("/vitoria", VitoriaController.create)
 routes.get("/vitoria/:_id", VitoriaController.byId)
 routes.get("/vitoria", VitoriaController.list)
@@ -39,6 +46,11 @@ routes.post("/vitoria-mini", VitoriaMiniController.create)
 routes.get("/vitoria-mini/:_id", VitoriaMiniController.byId)
 routes.get("/vitoria-mini", VitoriaMiniController.list)
 routes.put("/edit-vitoria-mini/:_id", VitoriaMiniController.edit)
+
+routes.post("/vitoria-code", VitoriaCodeController.create)
+routes.get("/vitoria-code/:_id", VitoriaCodeController.byId)
+routes.get("/vitoria-code", VitoriaCodeController.list)
+routes.put("/edit-vitoria-code/:_id", VitoriaCodeController.edit)
 
 routes.post("/volta", CorridaFollowController.create)
 routes.get("/volta", CorridaFollowController.list)
